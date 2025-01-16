c9534e75c92b5b72307f6d14249bb4764dea71d7
kiran <kiran.211720@ncit.edu.np>
2025-01-16 13:13:15 +0545

    basic variables

--- variables.py
+++ variables.py
@@ -1,9 +1,31 @@
 # x = "Sally"
 # x = 4
 # print(x)
-x = 4
-y = float(3)
-z = int(3.9)
-X = 5
-print(X) 
+#multiple variables at a time
+x,y,z = 2,3,4
+# print(x,y,z) 
+
+x = y = z = "Orange"
+# print(x[0])
+# print(y[1])
+# print(z[2])
+
+# unpacking the collection
+fruits = ["orange","apple","banana"]
+x = y = z = fruits
+# print(x,y,z)
+
+# global variables ko kura
+x = 6
+def myFunc():
+    x = 9
+    print(x+5)
+myFunc()
+print(x)
+
+def func():
+    x =3
+    x = 'fantastic'
+func()
+print(x)
 
