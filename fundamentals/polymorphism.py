d1364d94564081c1dda322afaf7cbc0699aacd55
kiran <kiran.211720@ncit.edu.np>
2025-01-19 21:25:23 +0545

    inheritance in polymorphism

--- fundamentals/polymorphism.py
+++ fundamentals/polymorphism.py
@@ -30,4 +30,35 @@ areo = areoplane("fly emirates","88480")
 
 for x in (car1,boat,areo):
     x.move()
+
+
+# Inheritance class polymorphism
+
+
+class Vechile:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    def move(self):
+        print("drive")
+
+class car(Vechile):
+    pass
+
+class Boat(Vechile):
+    def move(self):
+        print("sail")
+
+class aeroplane(Vechile):
+    def move(self):
+        print("fly")
+
+car1 = Car("toyota","first")
+boat = Boat("dont know","hello")
+areo = areoplane("fly emirates","88480")
+
+for x in (car1,boat,areo):
+    print(x.model)
+    print(x.brand)
+    x.move()
         
