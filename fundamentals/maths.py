804efabc307ae4687a5c7cb76caf45c3b4d2fe65
kiran <kiran.211720@ncit.edu.np>
2025-01-19 22:56:27 +0545

    maths in python

---
+++ fundamentals/maths.py
@@ -0,0 +1,26 @@
+x = min(2,3,4,5,0,7,5)
+y = max(19,34,12,5,456)
+print(x)
+print(y)
+
+# abs le positive value matrai return garxa
+k = -4353
+print(abs(k))
+
+# power check garna laii
+print(pow(2,7))
+# module pani xa math function which is already in built library
+
+import math as mth
+
+x = mth.sqrt(99)
+print(x)
+
+# ceil vanne function le round off garera value return garxa mathi lagxa
+
+x = mth.ceil(1.2)
+print(x)
+
+# floor canne function le roundoff garera down line garxa
+x = mth.floor(1.2)
+print(x)
