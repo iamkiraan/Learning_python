d6435d2bc1c2585a4ee4988ca40bcc1f3b00554b
kiran <kiran.211720@ncit.edu.np>
2025-01-19 23:03:44 +0545

    json in python

---
+++ fundamentals/Json.py
@@ -0,0 +1,13 @@
+# inbuilt library in python json to work with json
+
+import json
+
+x =  '{"name":"kiran acharya","age":23}'
+y = json.loads(x)
+print(y)
+
+x = {
+    "name":"kiran acharya",
+    "age":23
+}
+print(x)
