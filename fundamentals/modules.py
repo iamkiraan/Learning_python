4deb859d38a8fb3c94ad48803f6e848e58924cf7
kiran <kiran.211720@ncit.edu.np>
2025-01-19 22:27:37 +0545

    modules in python

---
+++ fundamentals/modules.py
@@ -0,0 +1,7 @@
+# modules in python
+# a set of function that you wanted to execute
+import myModule as m
+m.Greetings("hello there")
+
+a = m.person
+print(a)
---
+++ fundamentals/myModule.py
@@ -0,0 +1,8 @@
+def Greetings(name):
+    print(name)
+
+person ={
+    "name" : "kiran acharya",
+    "age": 23,
+    "contact" : 9865445343
+}
